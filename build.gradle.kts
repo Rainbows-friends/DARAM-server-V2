a3bc02ac555a75d4dd0ae8db54d21c6ee9b57f43
snowykte0426 <snowykte0426@naver.com>
2024-10-22 20:47:01 +0900

    chore: AWS 의존성 및 필드 제거
    AWS 관련 기능의 제거로 필요 없어진 의존성과 필드를 제거했습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,23 +18,56 @@ java {
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven { url = uri("https://jitpack.io") }
 }
 
 extra["snippetsDir"] = file("build/generated-snippets")
 
 dependencies {
+    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Database (DB 관련)
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    // Kotlin & Jackson
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Cache
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.ehcache:ehcache")
+    implementation("javax.cache:cache-api")
+
+    // JAXB
+    implementation("javax.xml.bind:jaxb-api")
+    implementation("org.glassfish.jaxb:jaxb-runtime")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-stater-webmvc-ui")
+
+    // Thymeleaf Extras
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api")
+    implementation("io.jsonwebtoken:jjwt-impl")
+    implementation("io.jsonwebtoken:jjwt-jackson")
+
+    // Development Only
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    // Performance Monitoring
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
