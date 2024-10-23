cfaf30242bdf5e336ff232c88f344d3068e5b02e
snowykte0426 <snowykte0426@naver.com>
2024-10-23 14:33:47 +0900

    feat: Redis CRUD 동작 클래스 추가
    Redis에서 값을 설정하고,읽어오고,삭제하는 클래스를 정의하고 구현했습니다

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/redis/RedisUtil.kt
@@ -0,0 +1,23 @@
+package rainbowfriends.daramserverv2.global.redis
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class RedisUtil(
+    private val redisTemplate: RedisTemplate<String, Any>
+) {
+
+    fun set(key: String, value: Any, minutes: Int) {
+        redisTemplate.opsForValue().set(key, value, minutes.toLong(), TimeUnit.MINUTES)
+    }
+
+    fun get(key: String): Any? {
+        return redisTemplate.opsForValue().get(key)
+    }
+
+    fun delete(key: String): Boolean {
+        return redisTemplate.delete(key)
+    }
+}
