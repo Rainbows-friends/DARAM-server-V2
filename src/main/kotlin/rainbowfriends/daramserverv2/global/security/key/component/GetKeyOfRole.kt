4bd8f75f14b19ce784a9bf3c053d50375dd3c376
snowykte0426 <snowykte0426@naver.com>
2024-10-24 01:13:17 +0900

    feat: Key 권한 조회 메서드 작성
    Key의 권한을 조회하는 메서드를 정의하였습니다
    
    Resolves: #9
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/key/component/GetKeyOfRole.kt
@@ -0,0 +1,14 @@
+package rainbowfriends.daramserverv2.global.security.key.component
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import rainbowfriends.daramserverv2.global.member.enums.Roles
+import rainbowfriends.daramserverv2.global.security.key.repository.KeyRepository
+
+@Component
+@Transactional
+class GetKeyOfRole(private val keyRepository: KeyRepository) {
+    fun getKeyOfRole(key: String):Roles {
+        return keyRepository.findByKey(key).role
+    }
+}
