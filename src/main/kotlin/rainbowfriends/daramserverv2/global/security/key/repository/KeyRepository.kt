40da6c6002fa9386c79473a68bac3889eea3c74c
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:53:08 +0900

    feat: Key 레포지터리 정의
    Key 테이블과 트랜젝션을 담당하는 레포지터리를 정의하였습니다
    
    Resolves: #9
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/key/repository/KeyRepository.kt
@@ -0,0 +1,11 @@
+package rainbowfriends.daramserverv2.global.security.key.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import rainbowfriends.daramserverv2.global.security.key.entity.Key
+
+@Repository
+interface KeyRepository : JpaRepository<Key, String> {
+    fun existsByKey(key: String): Boolean
+    fun findByKey(key: String): Key
+}
