0bbaf32e27e28da0b987b1da9449cf2da71d19d0
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:36:24 +0900

    feat: ``@EntityScan``,``@EnableRedisRepositories``어노테이션 적용
    Redis 레포지터리와 엔티티를 인식하기 위하여 어노테이션으로 수동으로 경로를 설정하였습니다
    
    Resolves: #1, #9

--- src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
@@ -1,11 +1,14 @@
 package rainbowfriends.daramserverv2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
+@EnableRedisRepositories(basePackages = ["rainbowfriends.daramserverv2.global.security.repository"])
+@EntityScan(basePackages = ["rainbowfriends.daramserverv2.global.security.key.entity"])
 class DaramServerV2Application
-
 fun main(args: Array<String>) {
-    runApplication<DaramServerV2Application>(*args)
+        runApplication<DaramServerV2Application>(*args)
 }
