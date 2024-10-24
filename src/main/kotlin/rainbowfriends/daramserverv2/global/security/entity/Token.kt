aebdb31c164eb3f3523b8fbaa2ebd9c0539b4463
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:54:23 +0900

    feat: Token Hash 엔티티 정의
    발행된 Token을 Redis에 저장하는 엔티티를 정의했습니다
    
    Resolves: #9
    Related to: #1

--- src/main/kotlin/rainbowfriends/daramserverv2/global/security/entity/Token.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/entity/Token.kt
@@ -3,11 +3,12 @@ package rainbowfriends.daramserverv2.global.security.entity
 import jakarta.persistence.Id
 import org.springframework.data.redis.core.RedisHash
 import rainbowfriends.daramserverv2.global.member.enums.Roles
-import java.time.LocalDateTime
+import java.util.*
 
 @RedisHash("token", timeToLive = 60 * 60 * 24)
 data class Token(
-    @Id val token: String,
+    @Id val id: String = UUID.randomUUID().toString(),
+    var token: String,
     var role: Roles,
-    var expiredAt: LocalDateTime
+    var expiredAt: Date
 )
