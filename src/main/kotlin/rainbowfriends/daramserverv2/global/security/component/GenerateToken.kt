664fecada7ff55b379b670e1cde5c2ee078f9155
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:49:58 +0900

    feat: Token 발행 로직 구현
    Token을 랜덤 문자열과 권한 정보를 포함하여 발행하는 로직을 구현하였습니다
    
    Resolves: #9
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/component/GenerateToken.kt
@@ -0,0 +1,50 @@
+package rainbowfriends.daramserverv2.global.security.component
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import rainbowfriends.daramserverv2.global.member.enums.Roles
+import rainbowfriends.daramserverv2.global.redis.RedisUtil
+import rainbowfriends.daramserverv2.global.security.entity.Token
+import rainbowfriends.daramserverv2.global.security.repository.TokenRepository
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.*
+
+@Component
+class GenerateToken(
+    private val tokenRepository: TokenRepository,
+    private val redisUtil: RedisUtil,
+    @Value("\${token.secret}")
+    private val secret: String
+) {
+    private val random = SecureRandom()
+
+    private fun generateRandomBase64String(): String {
+        val randomBytes = ByteArray(24)
+        random.nextBytes(randomBytes)
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes)
+    }
+
+    private fun hashWithSecret(randomString: String, secret: String): String {
+        val message = randomString + secret
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(message.toByteArray(StandardCharsets.UTF_8))
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(hashBytes)
+    }
+
+    fun generateToken(roles: Roles): String {
+        var accessToken: String
+        do {
+            val randomBase64String = generateRandomBase64String()
+            accessToken = hashWithSecret(randomBase64String, secret)
+        } while (tokenRepository.existsByToken(accessToken))
+        val token = Token(
+            token = accessToken,
+            role = roles,
+            expiredAt = Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)
+        )
+        redisUtil.set(accessToken, token, 1440)
+        return accessToken
+    }
+}
