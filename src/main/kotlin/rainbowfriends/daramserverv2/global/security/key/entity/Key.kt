0ae0ce4d942810ea0f885f0c93415353d8d2f25b
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:52:40 +0900

    feat: Key 엔티티 정의
    로그인에 사용하는 Key 테이블을 정의하는 엔티티를 작성하였습니다
    
    Resolves: #9
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/key/entity/Key.kt
@@ -0,0 +1,14 @@
+package rainbowfriends.daramserverv2.global.security.key.entity
+
+import jakarta.persistence.*
+import rainbowfriends.daramserverv2.global.member.enums.Roles
+
+@Entity
+@Table(name = "key_table")
+data class Key(
+    @Id @Column(name = "`key`", unique = true)
+    val key: String,
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    val role: Roles
+)
