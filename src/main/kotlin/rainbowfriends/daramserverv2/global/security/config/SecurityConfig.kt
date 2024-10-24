f6f65ac4a24ed9fbf1f979acc51c0c94a33c9913
snowykte0426 <snowykte0426@naver.com>
2024-10-24 19:53:39 +0900

    feat: 보안 설정 클래스 구현
    보안 설정을 주입하는 클래스를 구현했습니다
    
    Resolves: #9
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package rainbowfriends.daramserverv2.global.security.config
+
+import jakarta.servlet.Filter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import rainbowfriends.daramserverv2.global.security.filter.TokenFilter
+import rainbowfriends.daramserverv2.global.security.token.TokenService
+
+@Configuration
+class SecurityConfig(
+    private val domainAuthorizationConfig: DomainAuthorizationConfig,
+    private val corsConfig: CorsConfig,
+) {
+
+    @Bean
+    fun securityFilterChain(
+        http: HttpSecurity, jwtTokenService: TokenService
+    ): SecurityFilterChain {
+        http
+            .cors { cors -> cors.configurationSource(corsConfig.configureCors()) }
+            .csrf { csrf -> csrf.disable() }
+            .formLogin { form -> form.disable() }
+            .authorizeHttpRequests { domainAuthorizationConfig.configure(it) }
+            .addFilterBefore(
+                TokenFilter(jwtTokenService) as Filter,
+                UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
+    }
+}
