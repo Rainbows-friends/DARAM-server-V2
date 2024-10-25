640e3dcf178d1e23990129ea07ba795f0ce74f06
snowykte0426 <snowykte0426@naver.com>
2024-10-26 04:24:32 +0900

    feat: 시간 관련 엔트포인트 정의
    기숙사 입사 시간까지 남은 시간을 반환하기 위한 시간 관련 API를 구현하기 위해 엔드포인트를 정의하였습니다
    
    Related to: #1

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/time/controller/TimeController.kt
@@ -0,0 +1,21 @@
+package rainbowfriends.daramserverv2.domain.time.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import rainbowfriends.daramserverv2.domain.time.dto.enums.ResponseType
+import rainbowfriends.daramserverv2.domain.time.service.RemainTimeService
+
+@Tag(name = "Time", description = "시간 API")
+@RestController
+@RequestMapping("/time")
+class TimeController(private val remainTimeService: RemainTimeService) {
+    @Operation(summary = "입사 마감 남은 시간 조회", description = "입사 마감까지 남은 시간을 조회합니다")
+    @GetMapping("/remaintime")
+    fun getRemainTime(@RequestParam(name = "responsetype") responseType: String): Any {
+        return remainTimeService.getRemainTime(ResponseType.from(responseType) )
+    }
+}
