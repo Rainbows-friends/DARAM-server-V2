836e52682d85e45c994fff166dbe63b5ef32fb0d
snowykte0426 <snowykte0426@naver.com>
2024-10-27 16:50:08 +0900

    refactor: ``resolveToken`` 메서드 삭제와 메서드 역할 변경에 따른 리팩터링
    ``resolveToken`` 메서드 삭제와 메서드 역할 변경에 따라 기존 기능을 그대로 수행하도록 리팩터링 하였습니다
    
    Resolves: #12
    Related to: #9

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/notice/service/impl/PostNoticeServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/notice/service/impl/PostNoticeServiceImpl.kt
@@ -6,13 +6,16 @@ import rainbowfriends.daramserverv2.domain.notice.component.SaveNotice
 import rainbowfriends.daramserverv2.domain.notice.dto.request.CreateOrUpdateNoticeRequest
 import rainbowfriends.daramserverv2.domain.notice.entity.Notice
 import rainbowfriends.daramserverv2.domain.notice.service.PostNoticeService
-import rainbowfriends.daramserverv2.global.security.token.TokenService
+import rainbowfriends.daramserverv2.global.security.token.TokenAuthenticationService
 
 @Service
-class PostNoticeServiceImpl(private val tokenService: TokenService, private val saveNotice: SaveNotice) :
+class PostNoticeServiceImpl(
+    private val tokenAuthenticationService: TokenAuthenticationService,
+    private val saveNotice: SaveNotice
+) :
     PostNoticeService {
     override fun postNotice(request: HttpServletRequest, requestDto: CreateOrUpdateNoticeRequest): Notice {
-        val role = tokenService.decodeToken(tokenService.resolveToken(request.getHeader("Authorization")))
+        val role = tokenAuthenticationService.getRoleByToken(request.getHeader("Authorization"))
         return saveNotice.saveNotice(requestDto, role)
     }
 }
