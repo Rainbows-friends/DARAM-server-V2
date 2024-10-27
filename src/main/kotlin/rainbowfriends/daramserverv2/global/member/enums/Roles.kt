bf706b3cc2fa6bfbc8d568bf1e9d5311b2036b04
snowykte0426 <snowykte0426@naver.com>
2024-10-27 16:41:13 +0900

    fix: 권한 enum 클래스 변경
    Spring Security에서 권한을 올바르게 인식하도록 단순한 enum 클래스에서 세부적인 설정값들을 지정하여 변경하였고 ``STUDENT``를 ``USER``로 변경하여 일반 권한을 가진 것임을 명확히 하였습니다
    
    Resolves: #12
    Related to: #9

--- src/main/kotlin/rainbowfriends/daramserverv2/global/member/enums/Roles.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/member/enums/Roles.kt
@@ -1,5 +1,47 @@
 package rainbowfriends.daramserverv2.global.member.enums
 
-enum class Roles {
-    ADMIN, STUDENT, TEACHER, DEVELOPER
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+
+enum class Roles : Authentication, GrantedAuthority {
+    ADMIN {
+        override fun getAuthorities() = setOf(this)
+        override fun getCredentials() = null
+        override fun getDetails() = null
+        override fun getPrincipal() = this
+        override fun isAuthenticated() = true
+        override fun setAuthenticated(isAuthenticated: Boolean) = Unit
+        override fun getName() = "ADMIN"
+    },
+    DEVELOPER {
+        override fun getAuthorities() = setOf(this)
+        override fun getCredentials() = null
+        override fun getDetails() = null
+        override fun getPrincipal() = this
+        override fun isAuthenticated() = true
+        override fun setAuthenticated(isAuthenticated: Boolean) = Unit
+        override fun getName() = "DEVELOPER"
+    },
+    TEACHER {
+        override fun getAuthorities() = setOf(this)
+        override fun getCredentials() = null
+        override fun getDetails() = null
+        override fun getPrincipal() = this
+        override fun isAuthenticated() = true
+        override fun setAuthenticated(isAuthenticated: Boolean) = Unit
+        override fun getName() = "TEACHER"
+    },
+    USER {
+        override fun getAuthorities() = setOf(this)
+        override fun getCredentials() = null
+        override fun getDetails() = null
+        override fun getPrincipal() = this
+        override fun isAuthenticated() = true
+        override fun setAuthenticated(isAuthenticated: Boolean) = Unit
+        override fun getName() = "USER"
+    };
+
+    override fun getAuthority(): String {
+        return name
+    }
 }
