83e7ad327beafcd886e24be4af6e5f738f641785
snowykte0426 <snowykte0426@naver.com>
2024-11-18 23:37:12 +0900

    feat: 지각 처리 메서드 구현
    오후 9시 30분까지 입사하지 않은 경우 지각 처리하는 메서드를 구현하였습니다
    
    Resolves: #15

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/CheckInDataManger.kt
@@ -0,0 +1,54 @@
+package rainbowfriends.daramserverv2.global.checkin.component
+
+import org.springframework.cache.Cache
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import rainbowfriends.daramserverv2.global.cache.config.CacheConfig
+import rainbowfriends.daramserverv2.global.checkin.entity.CheckIn
+import rainbowfriends.daramserverv2.global.checkin.exception.DateCalculationException
+import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
+import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
+import rainbowfriends.daramserverv2.global.member.component.MemberDataSync
+import rainbowfriends.daramserverv2.global.member.entity.Member
+import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+@Component
+class CheckInDataManger(
+    private val checkInRepository: CheckInRepository,
+    private val checkInMongoDBRepository: CheckInMongoDBRepository,
+    private val memberRepository: MemberRepository,
+    private val memberDataSync: MemberDataSync,
+    private val checkInDataSync: CheckInDataSync,
+    private val cacheConfig: CacheConfig
+) {
+    @Scheduled(cron = "0 30 21 * * *")
+    fun scheduledCheckInDataSync() {
+        val today: LocalDate? = LocalDate.now()
+        val tomorrow: LocalDate? = today?.plusDays(1)
+        println(tomorrow)
+        if (today == null || tomorrow == null) {
+            throw DateCalculationException("Date calculation failed")
+        }
+        if (today.dayOfWeek == DayOfWeek.FRIDAY) {
+            return
+        }
+        val cache: Cache? = cacheConfig.cacheManager().getCache("allMembers")
+        val allMember: List<Any?> = cache?.get("allMembers")?.get() as? List<*> ?: memberRepository.findAll()
+        if (checkInMongoDBRepository.findByCheckinDate(tomorrow).isEmpty()) {
+            allMember.forEach {
+                if (it is Member) {
+                    val checkIn = CheckIn(
+                        id = null,
+                        user = it,
+                        checkinDate = tomorrow,
+                        checkinStatus = false
+                    )
+                    checkInRepository.save(checkIn)
+                    checkInDataSync.syncCheckInToMongoDB(checkIn)
+                }
+            }
+        }
+    }
+}
