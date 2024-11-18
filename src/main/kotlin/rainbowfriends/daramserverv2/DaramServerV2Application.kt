2cf228da15d73a0c0faad495c9c09f8a825b1e63
snowykte0426 <snowykte0426@naver.com>
2024-11-18 20:33:06 +0900

    feat: ``@EntityScan``,``@EnableElasticsearchRepositories``어노테이션 추가
    ``@EntityScan``,``@EnableElasticsearchRepositories`` 어노테이션을 추가하여 Entity 위치를 지정해주고 Elasticsearch 레포지터리 위치를 명시하였습니다
    
    Resolves: #17

--- src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
@@ -3,9 +3,18 @@ package rainbowfriends.daramserverv2
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 @SpringBootApplication
-@EntityScan(basePackages = ["rainbowfriends.daramserverv2.global.security.key.entity", "rainbowfriends.daramserverv2.domain.notice.entity"])
+@EnableElasticsearchRepositories(basePackages = ["rainbowfriends.daramserverv2.global.member.repository"])
+@EntityScan(
+    basePackages = [
+        "rainbowfriends.daramserverv2.global.security.key.entity",
+        "rainbowfriends.daramserverv2.domain.notice.entity",
+        "rainbowfriends.daramserverv2.global.member.entity",
+        "rainbowfriends.daramserverv2.global.checkin.entity"
+    ]
+)
 class DaramServerV2Application
 
 fun main(args: Array<String>) {
