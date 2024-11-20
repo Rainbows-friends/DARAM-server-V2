ec8a0af49422c56a6743e3757be6f7087c61ed75
snowykte0426 <snowykte0426@naver.com>
2024-11-20 15:01:24 +0900

    feat: CheckInDTO 전환 메서드 구현
    CheckIn Data Class를 CheckInDTO로 전환하는 메서드를 구현하였습니다
    
    Resolves: #15

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckIn.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckIn.kt
@@ -1,27 +1,36 @@
 package rainbowfriends.daramserverv2.global.checkin.entity
 
 import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import rainbowfriends.daramserverv2.global.checkin.dto.CheckInDTO
 import rainbowfriends.daramserverv2.global.member.entity.Member
 import java.time.LocalDate
 
 @Entity
-@Table(name = "checkin")
+@Table(name = "checkin_table")
 data class CheckIn(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
-    @ManyToOne(fetch = FetchType.LAZY)
+    val id: Long?,
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     val user: Member,
     @Column(name = "checkin_status", nullable = false)
     var checkinStatus: Boolean = false,
     @Column(name = "checkin_date", nullable = false)
     val checkinDate: LocalDate = LocalDate.now()
 ) {
-    fun toMongoDBDocument(): CheckInMongoDB {
-        return CheckInMongoDB(
-            id = this.id.toString(),
-            user = this.user,
+    fun toDTO(): CheckInDTO {
+        return CheckInDTO(
+            id = this.id,
+            userName = this.user.name,
+            studentId = this.user.generateStudentId(
+                grade = this.user.grade,
+                classNum = this.user.classNum,
+                number = this.user.number
+            ),
             checkinStatus = this.checkinStatus,
             checkinDate = this.checkinDate
         )
