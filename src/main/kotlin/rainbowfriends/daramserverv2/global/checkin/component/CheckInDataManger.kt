d7d3de5ea02eb52963c19e45cd9ef06b9dfbe49f
snowykte0426 <snowykte0426@naver.com>
2024-11-20 15:22:01 +0900

    feat: CheckIn 데이터 초기화 기능 추가
    객체 생성 관련된 문제를 해결하고 데이터를 초기화하는 기능을 완성하였습니다
    
    Resolves: #15

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/CheckInDataManger.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/CheckInDataManger.kt
@@ -1,11 +1,11 @@
 package rainbowfriends.daramserverv2.global.checkin.component
 
-import org.springframework.cache.Cache
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import rainbowfriends.daramserverv2.global.cache.config.CacheConfig
-import rainbowfriends.daramserverv2.global.checkin.entity.CheckIn
-import rainbowfriends.daramserverv2.global.checkin.exception.DateCalculationException
+import rainbowfriends.daramserverv2.global.checkin.event.CheckInDataSyncCompletedEvent
+import rainbowfriends.daramserverv2.global.checkin.exception.LateNumberRaiseFailException
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
 import rainbowfriends.daramserverv2.global.member.entity.Member
@@ -19,43 +19,41 @@ class CheckInDataManger(
     private val checkInMongoDBRepository: CheckInMongoDBRepository,
     private val memberRepository: MemberRepository,
     private val checkInDataSync: CheckInDataSync,
-    private val cacheConfig: CacheConfig
+    private val cacheConfig: CacheConfig,
+    private val checkInPreparation: CheckInPreparation
 ) {
+
+    @EventListener(CheckInDataSyncCompletedEvent::class)
+    fun initializeCheckInData() {
+        val today = LocalDate.now()
+        val tomorrow = today.plusDays(1)
+        checkInPreparation.prepareCheckInsForDate(today)
+        checkInPreparation.prepareCheckInsForDate(tomorrow)
+    }
+
     @Scheduled(cron = "0 30 21 * * *")
     fun scheduledCheckInDataSync() {
-        val today: LocalDate? = LocalDate.now()
-        val tomorrow: LocalDate? = today?.plusDays(1)
+        val today: LocalDate = LocalDate.now()
+        val tomorrow: LocalDate = today.plusDays(1)
         println(tomorrow)
-        if (today == null || tomorrow == null) {
-            throw DateCalculationException("Date calculation failed")
-        }
         if (today.dayOfWeek == DayOfWeek.FRIDAY) {
             return
         }
-        val cache: Cache? = cacheConfig.cacheManager().getCache("allMembers")
-        val allMember: Iterable<Any?> =
-            cache?.get("allMembers")?.get() as? List<*> ?: memberRepository.findAll()
-        if (checkInMongoDBRepository.findByCheckinDate(tomorrow).isEmpty()) {
-            allMember.forEach {
-                if (it is Member) {
-                    val checkIn = CheckIn(
-                        id = null,
-                        user = it,
-                        checkinDate = tomorrow,
-                        checkinStatus = false
-                    )
-                    checkInRepository.save(checkIn)
-                    checkInDataSync.syncCheckInToMongoDB(checkIn)
-                }
-            }
-        }
-        checkInMongoDBRepository.findByCheckinDate(today).forEach {
-            it.user.id?.let { it1 ->
-                memberRepository.findById(it1).ifPresent { member ->
-                    member.lateNumber += 1
+        checkInPreparation.prepareCheckInsForDate(tomorrow)
+        lateNumberRaise(today)
+    }
+
+    private fun lateNumberRaise(today: LocalDate) {
+        try {
+            checkInRepository.findByCheckinDate(today).forEach { checkIn ->
+                if (checkIn.checkinStatus == false) {
+                    val member: Member = memberRepository.findById(checkIn.id!!).get()
+                    member.lateNumber.plus(1)
                     memberRepository.save(member)
                 }
             }
+        } catch (e: Exception) {
+            throw LateNumberRaiseFailException("Late Number Raise failed")
         }
     }
 }
