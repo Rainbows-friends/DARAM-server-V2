380861905acf1b1863cd82e77faedc36bd33e9b6
snowykte0426 <snowykte0426@naver.com>
2024-11-20 20:04:17 +0900

    feat: ``EnableMongoRepository`` 어노테이션 적용
    MongoDB Repository의 위치를 명시적으로 지정해주는 어노테이션을 적용해야해
    
    Resolves: #15

--- src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
@@ -3,6 +3,7 @@ package rainbowfriends.daramserverv2
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
 @EntityScan(
@@ -13,6 +14,7 @@ import org.springframework.boot.runApplication
         "rainbowfriends.daramserverv2.global.checkin.entity"
     ]
 )
+@EnableMongoRepositories(basePackages = ["rainbowfriends.daramserverv2.global.checkin.repository"])
 class DaramServerV2Application
 
 fun main(args: Array<String>) {
