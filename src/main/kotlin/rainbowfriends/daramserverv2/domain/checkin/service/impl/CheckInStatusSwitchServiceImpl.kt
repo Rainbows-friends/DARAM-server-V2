dc6204853cc02ace2e6b92cc52b15157eeb746e6
snowykte0426 <snowykte0426@naver.com>
2024-11-21 23:49:01 +0900

    feat: 입실 상태 변경 로직 구현
    당일 특정 Member의 입실상태를 변경하는 로직을 구현하였습니다
    
    Resolves: #1, #15

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/CheckInStatusSwitchServiceImpl.kt
@@ -0,0 +1,20 @@
+package rainbowfriends.daramserverv2.domain.checkin.service.impl
+
+import org.springframework.stereotype.Service
+import rainbowfriends.daramserverv2.domain.checkin.component.CheckInStatusSwitch
+import rainbowfriends.daramserverv2.domain.checkin.dto.request.CheckInStatusSwitchRequest
+import rainbowfriends.daramserverv2.domain.checkin.service.CheckInStatusSwitchService
+import rainbowfriends.daramserverv2.global.checkin.exception.CheckInStatusSwitchException
+import java.time.LocalDate
+
+@Service
+class CheckInStatusSwitchServiceImpl(private val checkInStatusSwitch: CheckInStatusSwitch): CheckInStatusSwitchService {
+    override fun switchCheckInStatus(request: CheckInStatusSwitchRequest) {
+        if (!checkInStatusSwitch.switchCheckInStatus(
+            date = LocalDate.now(),
+            username = request.name
+        )){
+            throw CheckInStatusSwitchException("CheckIn Status Switch Failed")
+        }
+    }
+}
