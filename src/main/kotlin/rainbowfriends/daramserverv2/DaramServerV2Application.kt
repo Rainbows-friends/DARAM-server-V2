946ef2a3cf13edd4f0497ad70b906c9e31ce75f5
snowykte0426 <snowykte0426@naver.com>
2024-11-21 20:26:06 +0900

    feat: 스케쥴링 활성화
    ``@EnableScheduling`` 어노테이션을 적용하여 스케쥴링을 활성화하였습니다
    
    Resolves: #15

--- src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EntityScan(
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
     ]
 )
 @EnableMongoRepositories(basePackages = ["rainbowfriends.daramserverv2.global.checkin.repository"])
+@EnableScheduling
 class DaramServerV2Application
 
 fun main(args: Array<String>) {
