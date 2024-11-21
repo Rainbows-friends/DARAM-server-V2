1e4d81bf0aab65ec687f22ce1490bfb42c0e29f4
snowykte0426 <snowykte0426@naver.com>
2024-11-21 20:23:52 +0900

    feat: 지각 횟수 증분 로직 분리
    SRP 준수를 위해 해당 일자 오후 9시30분까지 입사하지 않은 Member의 ``lateNumber``를 증분시키도록 구현하였습니다
    
    Resolves: #15

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/LateNumberRaise.kt
@@ -0,0 +1,31 @@
+package rainbowfriends.daramserverv2.global.checkin.component
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
+import rainbowfriends.daramserverv2.global.checkin.exception.LateNumberRaiseFailException
+import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
+import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
+import java.time.LocalDate
+
+@Component
+class LateNumberRaise(
+    private val checkInRepository: CheckInRepository,
+    private val memberRepository: MemberRepository
+) {
+    @Transactional
+    fun lateNumberRaise(date: LocalDate) {
+        try {
+            checkInRepository.findByCheckinDate(date).forEach { checkIn ->
+                if (!checkIn.checkinStatus) {
+                    val member = memberRepository.findById(checkIn.id!!)
+                        .orElseThrow { MemberNotFoundException("Member not found") }
+                    member.lateNumber += 1
+                    memberRepository.save(member)
+                }
+            }
+        } catch (e: Exception) {
+            throw LateNumberRaiseFailException("Late Number Raise failed")
+        }
+    }
+}
