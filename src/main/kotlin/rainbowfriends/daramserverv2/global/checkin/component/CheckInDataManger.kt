6d0910349950b595e3fdd2b7adcb1a19f75b33db
snowykte0426 <snowykte0426@naver.com>
2024-11-21 20:16:19 +0900

    feat: 과거 데이터 삭제 및 메서드 컴포넌트화
    2일전의 입사 데이터는 삭제하도록 로직을 추가하였고 SRP를 준수하기 위하여 일부 메서드를 다른 클래스로 분리하였습니다
    
    Resolves: #15

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/CheckInDataManger.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/component/CheckInDataManger.kt
@@ -3,13 +3,9 @@ package rainbowfriends.daramserverv2.global.checkin.component
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import rainbowfriends.daramserverv2.global.cache.config.CacheConfig
 import rainbowfriends.daramserverv2.global.checkin.event.CheckInDataSyncCompletedEvent
-import rainbowfriends.daramserverv2.global.checkin.exception.LateNumberRaiseFailException
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
-import rainbowfriends.daramserverv2.global.member.entity.Member
-import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
 import java.time.DayOfWeek
 import java.time.LocalDate
 
@@ -17,9 +13,7 @@ import java.time.LocalDate
 class CheckInDataManger(
     private val checkInRepository: CheckInRepository,
     private val checkInMongoDBRepository: CheckInMongoDBRepository,
-    private val memberRepository: MemberRepository,
-    private val checkInDataSync: CheckInDataSync,
-    private val cacheConfig: CacheConfig,
+    private val lateNumberRaise: LateNumberRaise,
     private val checkInPreparation: CheckInPreparation
 ) {
 
@@ -31,29 +25,21 @@ class CheckInDataManger(
         checkInPreparation.prepareCheckInsForDate(tomorrow)
     }
 
+    @Scheduled(cron = "0 0 23 * * *")
+    fun deleteOldCheckInData() {
+        val cutoffDate = LocalDate.now().minusDays(2)
+        checkInRepository.deleteAll(checkInRepository.findByCheckinDate(cutoffDate))
+        checkInMongoDBRepository.deleteAll(checkInMongoDBRepository.findByCheckinDate(cutoffDate))
+    }
+
     @Scheduled(cron = "0 30 21 * * *")
     fun scheduledCheckInDataSync() {
         val today: LocalDate = LocalDate.now()
         val tomorrow: LocalDate = today.plusDays(1)
-        println(tomorrow)
         if (today.dayOfWeek == DayOfWeek.FRIDAY) {
             return
         }
         checkInPreparation.prepareCheckInsForDate(tomorrow)
-        lateNumberRaise(today)
-    }
-
-    private fun lateNumberRaise(today: LocalDate) {
-        try {
-            checkInRepository.findByCheckinDate(today).forEach { checkIn ->
-                if (checkIn.checkinStatus == false) {
-                    val member: Member = memberRepository.findById(checkIn.id!!).get()
-                    member.lateNumber.plus(1)
-                    memberRepository.save(member)
-                }
-            }
-        } catch (e: Exception) {
-            throw LateNumberRaiseFailException("Late Number Raise failed")
-        }
+        lateNumberRaise.lateNumberRaise(today)
     }
 }
