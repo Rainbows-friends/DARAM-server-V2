134f6b8841028de4764166468be44193a6e024b0
snowykte0426 <snowykte0426@naver.com>
2024-11-22 08:23:43 +0900

    feat: 당일 입실,미입실 Member 조회 및 상태전환 엔드포인트 정의
    당일에 입실,미입실 상태 Member를 각각 조회하고 상태를 전환할 수 있는 엔드포인트들을 정의하였습니다
    
    Resolves: #15

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/controller/CheckInController.kt
@@ -0,0 +1,39 @@
+package rainbowfriends.daramserverv2.domain.checkin.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import rainbowfriends.daramserverv2.domain.checkin.dto.request.CheckInStatusSwitchRequest
+import rainbowfriends.daramserverv2.domain.checkin.service.CheckInStatusSwitchService
+import rainbowfriends.daramserverv2.domain.checkin.service.CheckedInMemberService
+import rainbowfriends.daramserverv2.domain.checkin.service.MissedCheckInMemberService
+import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
+
+@RestController
+@RequestMapping("/checkin")
+@Tag(name = "CheckIn", description = "기숙사 입사 API")
+class CheckInController(
+    private val checkedInMemberService: CheckedInMemberService,
+    private val missedCheckInMemberService: MissedCheckInMemberService,
+    private val checkInStatusSwitchService: CheckInStatusSwitchService
+) {
+    @Operation(summary = "입실 상태 학생 조회", description = "입실 상태인 학생을 조회합니다")
+    @GetMapping("/checkin")
+    fun getCheckInMember(): List<CheckInMongoDB> {
+        return checkedInMemberService.getCheckedInMember()
+    }
+
+    @Operation(summary = "미입실 상태 학생 조회", description = "미입실 상태인 학생을 조회합니다")
+    @GetMapping("/uncheckin")
+    fun getUnCheckInMember(): List<CheckInMongoDB> {
+        return missedCheckInMemberService.getMissedCheckInMember()
+    }
+
+    @Operation(summary = "입실 상태 전환", description = "입실 상태를 전환합니다")
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun switchCheckInStatus(@RequestBody request: CheckInStatusSwitchRequest) {
+        checkInStatusSwitchService.switchCheckInStatus(request)
+    }
+}
