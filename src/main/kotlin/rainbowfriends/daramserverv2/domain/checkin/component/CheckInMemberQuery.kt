4c184ce33178ba7853ca1da3a40c95080275bd8d
snowykte0426 <snowykte0426@naver.com>
2024-11-22 11:45:27 +0900

    refactor: Member 정보 조회 로직 구현
    학번을 받아 그를 이용해 멤버 정보를 조회하는 메서드 ``getMemberInfo``를 추가하였습니다
    
    Resolves: #17
    Related to: #14

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInMemberQuery.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInMemberQuery.kt
@@ -2,13 +2,17 @@ package rainbowfriends.daramserverv2.domain.checkin.component
 
 import org.springframework.stereotype.Component
 import rainbowfriends.daramserverv2.domain.checkin.dto.GetCheckInComponentAction
+import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
+import rainbowfriends.daramserverv2.global.member.entity.Member
+import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
 import java.time.LocalDate
 
 @Component
 class CheckInMemberQuery(
-    private val checkInMongoDBRepository: CheckInMongoDBRepository
+    private val checkInMongoDBRepository: CheckInMongoDBRepository,
+    private val memberRepository: MemberRepository
 ) {
     fun getCheckInMember(action: GetCheckInComponentAction, date: LocalDate): List<CheckInMongoDB> {
         if (action == GetCheckInComponentAction.GET_CHECKED_IN_MEMBER) {
@@ -17,5 +21,15 @@ class CheckInMemberQuery(
             return checkInMongoDBRepository.findByCheckinDateAndCheckinStatus(date, false)
         }
         return checkInMongoDBRepository.findAll()
+    }
+
+    fun getMemberInfo(studentId: Short): Member {
+        studentId.toString().let {
+            val grade = it[0].toString().toInt()
+            val classNum = it[1].toString().toInt()
+            val studentNum = it.substring(2).toInt()
+            return memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, studentNum)
+                ?: throw MemberNotFoundException("Member not found")
+        }
     }
 }
