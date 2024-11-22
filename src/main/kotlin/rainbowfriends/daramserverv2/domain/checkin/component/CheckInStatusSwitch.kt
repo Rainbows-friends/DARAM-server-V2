2707569be7bb85a504bea1c82c43a0eb64faac84
snowykte0426 <snowykte0426@naver.com>
2024-11-22 17:50:13 +0900

    refactor: 분산 락 로직 추가
    ``switchCheckInStatus`` 메서드에 분산 락 로직을 추가하였습니다
    
    Resolves: #16

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInStatusSwitch.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInStatusSwitch.kt
@@ -5,25 +5,31 @@ import rainbowfriends.daramserverv2.global.checkin.component.CheckInDataSync
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
 import rainbowfriends.daramserverv2.global.member.entity.Member
 import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
+import rainbowfriends.daramserverv2.global.redis.redisson.util.DistributedLock
 import java.time.LocalDate
 
 @Component
 class CheckInStatusSwitch(
     private val checkInRepository: CheckInRepository,
     private val memberRepository: MemberRepository,
-    private val checkInDataSync: CheckInDataSync
+    private val checkInDataSync: CheckInDataSync,
+    private val distributedLock: DistributedLock
 ) {
+
     fun switchCheckInStatus(username: String?, date: LocalDate): Boolean {
-        try {
-            if (username == null) return false
-            val user: Member = memberRepository.findByName(username) ?: return false
-            val checkIn = checkInRepository.findByUserAndCheckinDate(user, date)
-            checkIn.checkinStatus = !checkIn.checkinStatus
-            checkInRepository.save(checkIn)
-            checkInDataSync.syncCheckInToMongoDB(checkIn)
-            return true
-        } catch (e: Exception) {
-            return false
+        if (username == null) return false
+        val lockKey = "check-in:$date"
+        return distributedLock.executeWithLock(lockKey, 5L, 10L) {
+            try {
+                val user: Member = memberRepository.findByName(username) ?: return@executeWithLock false
+                val checkIn = checkInRepository.findByUserAndCheckinDate(user, date)
+                checkIn.checkinStatus = !checkIn.checkinStatus
+                checkInRepository.save(checkIn)
+                checkInDataSync.syncCheckInToMongoDB(checkIn)
+                true
+            } catch (e: Exception) {
+                false
+            }
         }
     }
 }
