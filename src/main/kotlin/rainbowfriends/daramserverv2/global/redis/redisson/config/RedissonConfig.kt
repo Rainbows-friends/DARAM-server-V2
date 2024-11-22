ed5c5909f69768987dc70dec55d43436486c4d41
snowykte0426 <snowykte0426@naver.com>
2024-11-22 17:51:31 +0900

    feat: Redisson 설정 클래스 선언
    Redisson 라이브러리를 설정하는 클래스를 선언하였습니다
    
    Resolves: #16

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/redis/redisson/config/RedissonConfig.kt
@@ -0,0 +1,27 @@
+package rainbowfriends.daramserverv2.global.redis.redisson.config
+
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class RedissonConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private lateinit var redisHost: String
+
+    @Value("\${spring.data.redis.port}")
+    private lateinit var redisPort: String
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer()
+            .setAddress("redis://$redisHost:$redisPort")
+        return Redisson.create(config)
+    }
+}
