c41032927eaf3868062e157495234fb7ef916e96
snowykte0426 <snowykte0426@naver.com>
2024-11-22 11:46:30 +0900

    refactor: 불필요 의존성 제거
    불필요한 의존성을 제거하여 코드의 일관성을 높였습니다
    
    Resolves: #17

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckInMongoDB.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckInMongoDB.kt
@@ -1,9 +1,7 @@
 package rainbowfriends.daramserverv2.global.checkin.entity
 
 import jakarta.persistence.Id
-import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
-import rainbowfriends.daramserverv2.global.member.entity.Member
 import java.time.LocalDate
 
 @Document(collection = "checkin")
