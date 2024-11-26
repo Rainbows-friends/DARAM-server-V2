273adfc3f65adbf6712a41ea1001a5ad8938f997
snowykte0426 <snowykte0426@naver.com>
2024-11-26 09:24:42 +0900

    chore: ``/notice`` 엔드포인트 HTTP 메서드 별 권한 설정 세분화
    공지글 관련 엔드포인트의 접근 권한을 HTTP 메서드 별로 권한 설정을 세분화하였습니다
    
    Resolves: #1

--- src/main/kotlin/rainbowfriends/daramserverv2/global/security/config/DomainAuthorizationConfig.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/config/DomainAuthorizationConfig.kt
@@ -1,5 +1,6 @@
 package rainbowfriends.daramserverv2.global.security.config
 
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.stereotype.Component
@@ -9,13 +10,32 @@ class DomainAuthorizationConfig {
     fun configure(authorizeRequests: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry) {
         authorizeRequests
             .requestMatchers(
+                HttpMethod.GET,
+                "/notice/all",
+                "/notice/{id}"
+            ).permitAll()
+            .requestMatchers(
+                HttpMethod.POST,
+                "/notice/**"
+            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
+            .requestMatchers(
+                HttpMethod.PUT,
+                "/notice/**"
+            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
+            .requestMatchers(
+                HttpMethod.PATCH,
+                "/notice/**"
+            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
+            .requestMatchers(
+                HttpMethod.DELETE,
+                "/notice/**"
+            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
+            .requestMatchers(
                 "/swagger-ui/**",
-                "/graphiql/**",
-                "/actuator/**",
+                "/actuator/**"
             ).hasRole("DEVELOPER")
             .requestMatchers(
-                "/notice/**",
-                "/checkin",
+                "/checkin"
             ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
             .requestMatchers(
                 "/auth",
