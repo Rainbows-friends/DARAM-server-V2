5dc861134d82e0a93f932ec4838dd83822a39908
snowykte0426 <snowykte0426@naver.com>
2024-11-26 19:36:45 +0900

    chore: JVM 타임존 설정 추가
    JVM의 타임존을 설정하는 init 메서드를 구현하였습니다
    
    Resolves: #1

--- src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/DaramServerV2Application.kt
@@ -1,10 +1,12 @@
 package rainbowfriends.daramserverv2
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
 @SpringBootApplication
 @EntityScan(
@@ -21,4 +23,13 @@ class DaramServerV2Application
 
 fun main(args: Array<String>) {
     runApplication<DaramServerV2Application>(*args)
+    @PostConstruct
+    fun init() {
+        var timeZone = System.getenv("TZ");
+        if (timeZone == null || timeZone.isEmpty()) {
+            timeZone = "Asia/Seoul";
+        }
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+        System.out.println("Application running with timezone: " + timeZone);
+    }
 }
