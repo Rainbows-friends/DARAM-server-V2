ef3445c32327b000519f27a2c9962fd2dbcb6c29
snowykte0426 <snowykte0426@naver.com>
2024-11-30 20:12:33 +0900

    feat: CSP,OAuth 관련 설정 추가 및 Filter 순서 변경
    보안설정을 수정하여 JWT,OAuth 인가/인증 시스템에 알맞도록 수정하였습니다
    
    Resolves: #27

--- src/main/kotlin/rainbowfriends/daramserverv2/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/security/config/SecurityConfig.kt
@@ -1,13 +1,14 @@
 package rainbowfriends.daramserverv2.global.security.config
 
-import jakarta.servlet.Filter
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import rainbowfriends.daramserverv2.global.security.filter.TokenFilter
-import rainbowfriends.daramserverv2.global.security.token.TokenService
+import rainbowfriends.daramserverv2.global.security.jwt.filter.JwtFilter
+import rainbowfriends.daramserverv2.global.security.jwt.service.JwtTokenParserService
 
 @Configuration
 class SecurityConfig(
@@ -17,16 +18,39 @@ class SecurityConfig(
 
     @Bean
     fun securityFilterChain(
-        http: HttpSecurity, jwtTokenService: TokenService
+        http: HttpSecurity, jwtTokenParserService: JwtTokenParserService
     ): SecurityFilterChain {
         http
             .cors { cors -> cors.configurationSource(corsConfig.configureCors()) }
             .csrf { csrf -> csrf.disable() }
             .formLogin { form -> form.disable() }
+            .oauth2Login {
+                it.loginPage("/oauth2/authorization/google")
+                    .successHandler { request, response, authentication ->
+                        response.sendRedirect("/success")
+                    }
+                    .failureHandler { request, response, exception ->
+                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication Failed")
+                    }
+            }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
             .addFilterBefore(
-                TokenFilter(jwtTokenService) as Filter,
-                UsernamePasswordAuthenticationFilter::class.java)
-            .authorizeHttpRequests { domainAuthorizationConfig.configure(it) }
+                JwtFilter(jwtTokenParserService),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+            .authorizeHttpRequests {
+                domainAuthorizationConfig.configure(it)
+            }
+            .exceptionHandling { exceptions ->
+                exceptions.authenticationEntryPoint { request, response, authException ->
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                }
+                exceptions.accessDeniedHandler { request, response, accessDeniedException ->
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+                }
+            }
         return http.build()
     }
 }
