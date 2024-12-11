9953296a8117d0f5ec5474fb5639efccbccd493f
송재욱 <haensol976520@gmail.com>
2024-12-11 14:20:01 +0900

    Refactor: switchCheckInStatus 함수 분리
    기능별로 메소드(getMemberInfo, getCheckInRecord, toggleCheckInStatus)를 분리했어요. 메소드명은 나중에 컨벤션에 맞게 변경해주세요
    
    executeWithLock이 handleCheckInStatusSwitch 호출로만 구성되도록 했습니다. 일단 try catch를 handleCheckInStatusSwitch 안으로 예외처리 범위를 정했어요..

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInStatusSwitch.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInStatusSwitch.kt
@@ -1,12 +1,14 @@
 package rainbowfriends.daramserverv2.domain.checkin.component
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
-import rainbowfriends.daramserverv2.global.checkin.component.CheckInDataSync
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
+import rainbowfriends.daramserverv2.global.checkin.entity.CheckIn
 import rainbowfriends.daramserverv2.global.member.entity.Member
 import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
 import rainbowfriends.daramserverv2.global.redis.redisson.util.DistributedLock
+import rainbowfriends.daramserverv2.global.checkin.component.CheckInDataSync
 import java.time.LocalDate
 
 @Component
@@ -20,21 +22,43 @@ class CheckInStatusSwitch(
     fun switchCheckInStatus(studentId: Short?, date: LocalDate): Boolean {
         if (studentId == null) return false
         val lockKey = "check-in:$date"
-        val grade = studentId.toString()[0].toString().toInt()
-        val classNum = studentId.toString()[1].toString().toInt()
-        val number = studentId.toString().substring(2).toInt()
         return distributedLock.executeWithLock(lockKey, 5L, 10L) {
-            try {
-                val user: Member = memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, number)
-                    ?: throw MemberNotFoundException("Member not found")
-                val checkIn = checkInRepository.findByUserAndCheckinDate(user, date)
-                checkIn.checkinStatus = !checkIn.checkinStatus
-                checkInRepository.save(checkIn)
-                checkInDataSync.syncCheckInToMongoDB(checkIn)
-                true
-            } catch (e: Exception) {
-                false
-            }
+            handleCheckInStatusSwitch(studentId, date)
         }
+    }
+
+    private fun handleCheckInStatusSwitch(studentId: Short, date: LocalDate): Boolean {
+        return try {
+            val user = getMemberInfo(studentId)
+            val checkIn = getCheckInRecord(user, date)
+            toggleCheckInStatus(checkIn)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    @Transactional
+    fun getMemberInfo(studentId: Short): Member {
+        studentId.toString().let {
+            val grade = it[0].toString().toInt()
+            val classNum = it[1].toString().toInt()
+            val studentNum = it.substring(2).toInt()
+            return memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, studentNum)
+                ?: throw MemberNotFoundException("Member not found")
+        }
+    }
+
+    @Transactional
+    fun getCheckInRecord(user: Member, date: LocalDate): CheckIn {
+        return checkInRepository.findByUserAndCheckinDate(user, date)
+            ?: throw IllegalStateException("Check-in record not found")
+    }
+
+    @Transactional
+    fun toggleCheckInStatus(checkIn: CheckIn) {
+        checkIn.checkinStatus = !checkIn.checkinStatus
+        checkInRepository.save(checkIn)
+        checkInDataSync.syncCheckInToMongoDB(checkIn)
     }
 }
