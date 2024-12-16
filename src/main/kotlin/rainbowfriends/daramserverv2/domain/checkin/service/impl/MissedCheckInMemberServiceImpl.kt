205bcf66c57042da796ec6f683cf554fec5a96e6
snowykte0426 <snowykte0426@naver.com>
2024-12-16 19:38:34 +0900

    fix: 동일 객체 중복 반환 문제 해결
    이미 조회된 객체를 중복해서 조회해서 반환하던 문제를 해결하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/MissedCheckInMemberServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/MissedCheckInMemberServiceImpl.kt
@@ -6,24 +6,30 @@ import rainbowfriends.daramserverv2.domain.checkin.dto.enums.GetCheckInComponent
 import rainbowfriends.daramserverv2.domain.checkin.dto.response.GetCheckInResponse
 import rainbowfriends.daramserverv2.domain.checkin.service.MissedCheckInMemberService
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
+import rainbowfriends.daramserverv2.global.member.component.FindMember
 import rainbowfriends.daramserverv2.global.member.entity.Member
+import rainbowfriends.daramserverv2.global.util.ParseStudentId.parseStudentId
 import java.time.LocalDate
 
 @Service
-class MissedCheckInMemberServiceImpl(private val checkInMemberQuery: CheckInMemberQuery) : MissedCheckInMemberService {
+class MissedCheckInMemberServiceImpl(
+    private val checkInMemberQuery: CheckInMemberQuery,
+    private val findMember: FindMember
+) : MissedCheckInMemberService {
     override fun getMissedCheckInMember(): List<GetCheckInResponse> {
-        val checkInData: List<CheckInMongoDB> = checkInMemberQuery.getCheckInMemberWithLock(
+        val checkInData: List<CheckInMongoDB> = checkInMemberQuery.getCheckInMember(
             GetCheckInComponentAction.GET_MISSED_CHECK_IN_MEMBER,
             LocalDate.now()
         )
         return checkInData.map { mongoCheckIn ->
-            val memberInfo: Member = checkInMemberQuery.getMemberInfo(mongoCheckIn.studentId)
+            val (grade, classNum, number) = parseStudentId(mongoCheckIn.studentId.toString())
+            val memberInfo: Member = findMember.findMemberByGradeAndClassNumAndNumber(grade, classNum, number)!!
             GetCheckInResponse(
                 id = mongoCheckIn.id!!.toLong(),
                 user = memberInfo,
                 checkinStatus = mongoCheckIn.checkinStatus,
                 checkinDate = mongoCheckIn.checkinDate
             )
-        }
+        }.distinctBy { it.user }
     }
 }
