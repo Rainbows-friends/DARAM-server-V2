f9bb05f3366b2077bb83f5216815c7ae571e133a
snowykte0426 <snowykte0426@naver.com>
2024-12-16 18:20:53 +0900

    refactor: 책임 분리에 따른 리팩터링
    Member를 조회하는 책임을 타 컴포넌트로 분리하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInMemberQuery.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInMemberQuery.kt
@@ -1,31 +1,14 @@
 package rainbowfriends.daramserverv2.domain.checkin.component
 
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 import rainbowfriends.daramserverv2.domain.checkin.dto.enums.GetCheckInComponentAction
-import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
-import rainbowfriends.daramserverv2.global.member.entity.Member
-import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
-import rainbowfriends.daramserverv2.global.redis.redisson.util.DistributedLock
 import java.time.LocalDate
 
 @Component
-class CheckInMemberQuery(
-    private val checkInMongoDBRepository: CheckInMongoDBRepository,
-    private val memberRepository: MemberRepository,
-    private val distributedLock: DistributedLock
-) {
-
-    fun getCheckInMemberWithLock(action: GetCheckInComponentAction, date: LocalDate): List<CheckInMongoDB> {
-        val lockKey = "check-in-lock:$date"
-        return distributedLock.executeWithLock(lockKey, 5L, 10L) {
-            getCheckInMember(action, date)
-        }
-    }
-
-    private fun getCheckInMember(action: GetCheckInComponentAction, date: LocalDate): List<CheckInMongoDB> =
+class CheckInMemberQuery(private val checkInMongoDBRepository: CheckInMongoDBRepository) {
+    fun getCheckInMember(action: GetCheckInComponentAction, date: LocalDate): List<CheckInMongoDB> =
         when (action) {
             GetCheckInComponentAction.GET_CHECKED_IN_MEMBER -> getCheckInMember(date)
             GetCheckInComponentAction.GET_MISSED_CHECK_IN_MEMBER -> getMissedCheckInMember(date)
@@ -37,16 +20,5 @@ class CheckInMemberQuery(
 
     private fun getCheckInMember(date: LocalDate): List<CheckInMongoDB> {
         return checkInMongoDBRepository.findByCheckinDateAndCheckinStatus(date, true)
-    }
-
-    @Transactional
-    fun getMemberInfo(studentId: Short): Member {
-        studentId.toString().let {
-            val grade = it[0].toString().toInt()
-            val classNum = it[1].toString().toInt()
-            val studentNum = it.substring(2).toInt()
-            return memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, studentNum)
-                ?: throw MemberNotFoundException("Member not found")
-        }
     }
 }
--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/CheckedInMemberServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/CheckedInMemberServiceImpl.kt
@@ -6,22 +6,23 @@ import rainbowfriends.daramserverv2.domain.checkin.dto.enums.GetCheckInComponent
 import rainbowfriends.daramserverv2.domain.checkin.dto.response.GetCheckInResponse
 import rainbowfriends.daramserverv2.domain.checkin.service.CheckedInMemberService
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
+import rainbowfriends.daramserverv2.global.member.component.FindMember
 import rainbowfriends.daramserverv2.global.member.entity.Member
+import rainbowfriends.daramserverv2.global.util.ParseStudentId.parseStudentId
 import java.time.LocalDate
 
 @Service
 class CheckedInMemberServiceImpl(
-    private val checkInMemberQuery: CheckInMemberQuery
+    private val checkInMemberQuery: CheckInMemberQuery,
+    private val findMember: FindMember
 ) : CheckedInMemberService {
     override fun getCheckedInMember(): List<GetCheckInResponse> {
-        val checkInData: List<CheckInMongoDB> = checkInMemberQuery.getCheckInMemberWithLock(
-            GetCheckInComponentAction.GET_CHECKED_IN_MEMBER,
-            LocalDate.now()
-        )
+        val checkInData: List<CheckInMongoDB> = checkInMemberQuery.getCheckInMember(GetCheckInComponentAction.GET_CHECKED_IN_MEMBER, LocalDate.now())
         return checkInData.map { mongoCheckIn ->
-            val memberInfo: Member = checkInMemberQuery.getMemberInfo(mongoCheckIn.studentId)
+            val (grade, classNum, number) = parseStudentId(mongoCheckIn.studentId.toString())
+            val memberInfo: Member = findMember.findMemberByGradeAndClassNumAndNumber(grade, classNum, number)!!
             GetCheckInResponse(
-                id = mongoCheckIn.id!!.toLong(),
+                id = mongoCheckIn.id?.toLong(),
                 user = memberInfo,
                 checkinStatus = mongoCheckIn.checkinStatus,
                 checkinDate = mongoCheckIn.checkinDate
