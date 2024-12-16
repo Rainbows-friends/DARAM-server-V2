47cb6217e68da836638fb82099bae94e6e5bad6a
snowykte0426 <snowykte0426@naver.com>
2024-12-16 18:22:28 +0900

    feat: ``ReadOnly Transactional`` 적용
    읽기 전용 트랜젝션 어노테이션을 적용하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/global/member/component/FindMember.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/member/component/FindMember.kt
@@ -1,10 +1,12 @@
 package rainbowfriends.daramserverv2.global.member.component
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import rainbowfriends.daramserverv2.global.member.entity.Member
 import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
 
 @Component
+@Transactional(readOnly = true)
 class FindMember(private val memberRepository: MemberRepository) {
     fun findMemberByEmail(email: String): Member? {
         return memberRepository.findByEmail(email)
