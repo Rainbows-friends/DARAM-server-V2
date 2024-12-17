6b789c087e2a134175fb3e0ae850a2f1388eba89
snowykte0426 <snowykte0426@naver.com>
2024-12-17 11:35:32 +0900

    refactor: 불필요 의존성 제거
    불필요 의존성을 제거하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/repository/CheckInMongoDBRepository.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/repository/CheckInMongoDBRepository.kt
@@ -1,10 +1,8 @@
 package rainbowfriends.daramserverv2.global.checkin.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
-import rainbowfriends.daramserverv2.global.member.entity.Member
 import java.time.LocalDate
 
 @Repository
