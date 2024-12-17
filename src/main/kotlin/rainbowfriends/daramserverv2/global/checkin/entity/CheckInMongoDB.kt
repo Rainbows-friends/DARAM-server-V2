a41013ab7247d3e91ffdb3dd72e40b259c7f1d3d
snowykte0426 <snowykte0426@naver.com>
2024-12-17 11:33:06 +0900

    feat ``toDto`` 메서드 추가
    Entity를 DTO로 변환하는 메서드를 구현하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckInMongoDB.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/checkin/entity/CheckInMongoDB.kt
@@ -2,6 +2,7 @@ package rainbowfriends.daramserverv2.global.checkin.entity
 
 import jakarta.persistence.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import rainbowfriends.daramserverv2.global.checkin.dto.CheckInDTO
 import java.time.LocalDate
 
 @Document(collection = "checkin")
@@ -11,4 +12,12 @@ data class CheckInMongoDB(
     var studentId: Short,
     var checkinStatus: Boolean,
     var checkinDate: LocalDate
-)
+){
+    fun toDto() = CheckInDTO(
+        id = id!!.toLong(),
+        userName = userName,
+        studentId = studentId,
+        checkinStatus = checkinStatus,
+        checkinDate = checkinDate
+    )
+}
