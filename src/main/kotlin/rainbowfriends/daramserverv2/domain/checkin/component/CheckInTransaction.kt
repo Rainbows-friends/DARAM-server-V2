80e92e55fc146b28f7ee73daf4429462a298630f
snowykte0426 <snowykte0426@naver.com>
2024-12-17 12:09:26 +0900

    refactor: ``readOnly`` 트랜젝션 적용 및 2중 호출 구조로 변경
    JPA 성능 확보 및 2층 호출 구조로 변경하여 데이터 영송
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInTransaction.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/component/CheckInTransaction.kt
@@ -3,33 +3,29 @@ package rainbowfriends.daramserverv2.domain.checkin.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import rainbowfriends.daramserverv2.global.checkin.entity.CheckIn
+import rainbowfriends.daramserverv2.global.checkin.repository.CheckInMongoDBRepository
 import rainbowfriends.daramserverv2.global.checkin.repository.CheckInRepository
 import rainbowfriends.daramserverv2.global.member.entity.Member
 import rainbowfriends.daramserverv2.global.member.repository.MemberRepository
-import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
+import rainbowfriends.daramserverv2.global.member.dto.MemberDTO
 import java.time.LocalDate
 
 @Service
-class CheckInTransactionService(
+class CheckInTransaction(
     private val checkInRepository: CheckInRepository,
+    private val checkInMongoDBRepository: CheckInMongoDBRepository,
     private val memberRepository: MemberRepository
 ) {
 
     @Transactional
-    fun getMemberInfo(studentId: Short): Member {
-        studentId.toString().let {
-            val grade = it[0].toString().toInt()
-            val classNum = it[1].toString().toInt()
-            val studentNum = it.substring(2).toInt()
-            return memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, studentNum)
-                ?: throw MemberNotFoundException("Member not found")
-        }
+    fun getMemberDto(grade: Int, classNum: Int, studentNum: Int): MemberDTO {
+        return memberRepository.findByGradeAndClassNumAndNumber(grade, classNum, studentNum)!!.toDto()
     }
 
-    @Transactional
-    fun getCheckInRecord(user: Member, date: LocalDate): CheckIn {
-        return checkInRepository.findByUserAndCheckinDate(user, date)
-            ?: throw IllegalStateException("Check-in record not found")
+    @Transactional(readOnly = true)
+    fun getCheckInRecord(user: MemberDTO, date: LocalDate): CheckIn {
+        val user: Member? = memberRepository.findByGradeAndClassNumAndNumber(user.grade, user.classNum, user.number)
+        return checkInRepository.findByUserAndCheckinDate(user!!, date)
     }
 
     @Transactional
