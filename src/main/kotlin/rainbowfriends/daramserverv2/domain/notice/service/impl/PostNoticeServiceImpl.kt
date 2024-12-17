7cd7c51d3429472ddf75591c52f1598fa33c2f64
snowykte0426 <snowykte0426@naver.com>
2024-12-17 18:29:45 +0900

    refactor: 인증 방식 변화에 따른 리팩터링
    Key 기반 인증에서 JWT 기반 인증방식으로 변경됨에 따라 조금의 코드 수정을 하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/notice/service/impl/PostNoticeServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/notice/service/impl/PostNoticeServiceImpl.kt
@@ -6,16 +6,16 @@ import rainbowfriends.daramserverv2.domain.notice.component.SaveNotice
 import rainbowfriends.daramserverv2.domain.notice.dto.request.PostNoticeRequest
 import rainbowfriends.daramserverv2.domain.notice.dto.response.NoticeResponse
 import rainbowfriends.daramserverv2.domain.notice.service.PostNoticeService
-import rainbowfriends.daramserverv2.global.security.token.TokenAuthenticationService
+import rainbowfriends.daramserverv2.global.security.jwt.service.impl.JwtTokenParserServiceImpl
 
 @Service
 class PostNoticeServiceImpl(
-    private val tokenAuthenticationService: TokenAuthenticationService,
+    private val jwtTokenParserServiceImpl: JwtTokenParserServiceImpl,
     private val saveNotice: SaveNotice
 ) :
     PostNoticeService {
     override fun postNotice(request: HttpServletRequest, requestDto: PostNoticeRequest): NoticeResponse {
-        val role = tokenAuthenticationService.getRoleByToken(request.getHeader("Authorization"))
+        val role = jwtTokenParserServiceImpl.extractRole(request.getHeader("Authorization").substringAfter("Bearer "))
         return saveNotice.saveNotice(requestDto, role)
     }
 }
