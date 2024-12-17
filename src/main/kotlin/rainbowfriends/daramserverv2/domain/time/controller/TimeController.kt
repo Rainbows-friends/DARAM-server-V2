205b7ffb121cb3b8019c95eab95b5b8a01407f7f
snowykte0426 <snowykte0426@naver.com>
2024-12-17 20:53:00 +0900

    fix: ``@RequestMapping`` 어노테이션 누락 문제 해결
    시간 관련 엔드포인트에 대하여 기존에 존재하던 엔드포인트 설정이 의도치 않게 누락되어 API 명세서와 차이점이 생기던 문제를 해결하였습니다
    
    Related to: #1

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/time/controller/TimeController.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/time/controller/TimeController.kt
@@ -1,12 +1,14 @@
 package rainbowfriends.daramserverv2.domain.time.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import rainbowfriends.daramserverv2.domain.time.dto.enums.GetRemainTimeServiceAction
 import rainbowfriends.daramserverv2.domain.time.service.RemainTimeService
 
 @RestController
+@RequestMapping("/time")
 class TimeController(private val remainTimeService: RemainTimeService) {
     @GetMapping("/remaintime")
     fun getRemainTime(@RequestParam(name = "responsetype") responseType: String): Any {
