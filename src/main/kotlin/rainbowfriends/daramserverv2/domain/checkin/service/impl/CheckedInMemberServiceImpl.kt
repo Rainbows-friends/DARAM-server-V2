48ba48e152afaabff4a2710e29c655989f440e19
snowykte0426 <snowykte0426@naver.com>
2024-12-17 18:23:14 +0900

    refactor: ``CheckedInMemberServiceImpl`` 내 응답 매핑 단순화
    응답에 DTO를 사용하도록 하였고 요구사항의 변화에 따라 반환형식과 내용 역시 소폭 수정하였습니다
    
    Resolves: #43

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/CheckedInMemberServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/service/impl/CheckedInMemberServiceImpl.kt
@@ -5,27 +5,34 @@ import rainbowfriends.daramserverv2.domain.checkin.component.CheckInMemberQuery
 import rainbowfriends.daramserverv2.domain.checkin.dto.enums.GetCheckInComponentAction
 import rainbowfriends.daramserverv2.domain.checkin.dto.response.GetCheckInResponse
 import rainbowfriends.daramserverv2.domain.checkin.service.CheckedInMemberService
-import rainbowfriends.daramserverv2.global.checkin.entity.CheckInMongoDB
-import rainbowfriends.daramserverv2.global.member.component.FindMember
-import rainbowfriends.daramserverv2.global.member.entity.Member
-import rainbowfriends.daramserverv2.global.util.ParseStudentId.parseStudentId
+import rainbowfriends.daramserverv2.global.checkin.dto.CheckInDTO
 import java.time.LocalDate
 
 @Service
-class CheckedInMemberServiceImpl(
-    private val checkInMemberQuery: CheckInMemberQuery,
-    private val findMember: FindMember
-) : CheckedInMemberService {
+class CheckedInMemberServiceImpl(private val checkInMemberQuery: CheckInMemberQuery) : CheckedInMemberService {
     override fun getCheckedInMember(): List<GetCheckInResponse> {
-        val checkInData: List<CheckInMongoDB> = checkInMemberQuery.getCheckInMember(GetCheckInComponentAction.GET_CHECKED_IN_MEMBER, LocalDate.now())
-        return checkInData.map { mongoCheckIn ->
-            val (grade, classNum, number) = parseStudentId(mongoCheckIn.studentId.toString())
-            val memberInfo: Member = findMember.findMemberByGradeAndClassNumAndNumber(grade, classNum, number)!!
+        val checkInData: List<CheckInDTO> = checkInMemberQuery.getCheckInMember(
+            GetCheckInComponentAction.GET_CHECKED_IN_MEMBER,
+            LocalDate.now()
+        ).map {
+            CheckInDTO(
+                id = it.id,
+                user = it.user,
+                checkinStatus = it.checkinStatus,
+                checkinDate = it.checkinDate
+            )
+        }
+        return checkInData.map { checkIn ->
             GetCheckInResponse(
-                id = mongoCheckIn.id?.toLong(),
-                user = memberInfo,
-                checkinStatus = mongoCheckIn.checkinStatus,
-                checkinDate = mongoCheckIn.checkinDate
+                id = checkIn.id,
+                user = checkIn.user.toDto(),
+                checkinStatus = checkIn.checkinStatus,
+                checkinDate = String.format(
+                    "%02d:%02d:%02d",
+                    checkIn.checkinDate?.hour,
+                    checkIn.checkinDate?.minute,
+                    checkIn.checkinDate?.second
+                )
             )
         }
     }
