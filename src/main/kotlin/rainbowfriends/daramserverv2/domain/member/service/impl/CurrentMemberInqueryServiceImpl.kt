daa6e37882f43f3db0e7061ca537b47523189310
snowykte0426 <snowykte0426@naver.com>
2024-12-23 15:13:52 +0900

    feat: 현재 로그인된 사용자 정보 확인 비즈니스 로직 구현
    현재 로그인된 사용자 정보 조회 비즈니스 로직 구현
    
    Resolves: #51

---
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/member/service/impl/CurrentMemberInqueryServiceImpl.kt
@@ -0,0 +1,24 @@
+package rainbowfriends.daramserverv2.domain.member.service.impl
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.stereotype.Service
+import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
+import rainbowfriends.daramserverv2.domain.member.service.CurrentMemberInqueryService
+import rainbowfriends.daramserverv2.global.member.component.FindMember
+import rainbowfriends.daramserverv2.global.member.dto.MemberDTO
+import rainbowfriends.daramserverv2.global.security.jwt.service.JwtTokenParserService
+
+@Service
+class CurrentMemberInqueryServiceImpl(
+    private val jwtTokenParserService: JwtTokenParserService,
+    private val findMember: FindMember
+) : CurrentMemberInqueryService {
+    override fun getCurrentMember(request: HttpServletRequest): MemberDTO {
+        return findMember.findMemberByEmail(
+            jwtTokenParserService.extractUserId(
+                request.getHeader("Authorization").substring(7)
+            )
+        )
+            ?.toDto() ?: throw MemberNotFoundException("Member not found")
+    }
+}
