5d17bb8b94d3dfc7950c03247dbdcdc9735651cf
snowykte0426 <snowykte0426@naver.com>
2024-12-26 16:47:21 +0900

    refactor: 헤더 검증 로직 추가
    헤더가 존재하는지 2중으로 확인하는 로직을 구현하여 안정성을 높였습니다
    
    Related to: #51

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/member/service/impl/CurrentMemberInqueryServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/member/service/impl/CurrentMemberInqueryServiceImpl.kt
@@ -3,6 +3,7 @@ package rainbowfriends.daramserverv2.domain.member.service.impl
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Service
 import rainbowfriends.daramserverv2.domain.member.exception.MemberNotFoundException
+import rainbowfriends.daramserverv2.domain.member.exception.TokenNotFoundException
 import rainbowfriends.daramserverv2.domain.member.service.CurrentMemberInqueryService
 import rainbowfriends.daramserverv2.global.member.component.FindMember
 import rainbowfriends.daramserverv2.global.member.dto.MemberDTO
@@ -14,11 +15,14 @@ class CurrentMemberInqueryServiceImpl(
     private val findMember: FindMember
 ) : CurrentMemberInqueryService {
     override fun getCurrentMember(request: HttpServletRequest): MemberDTO {
-        return findMember.findMemberByEmail(
-            jwtTokenParserService.extractUserId(
-                request.getHeader("Authorization").substring(7)
+        if (request.getHeader("Authorization") == null) {
+            throw TokenNotFoundException("Token not found")
+        } else
+            return findMember.findMemberByEmail(
+                jwtTokenParserService.extractUserId(
+                    request.getHeader("Authorization").substring(7)
+                )
             )
-        )
-            ?.toDto() ?: throw MemberNotFoundException("Member not found")
+                ?.toDto() ?: throw MemberNotFoundException("Member not found")
     }
 }
