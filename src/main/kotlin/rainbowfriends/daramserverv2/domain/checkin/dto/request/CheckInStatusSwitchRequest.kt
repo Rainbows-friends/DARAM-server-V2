31f0c24ecef6f37d8dfc1cd8a09b3b06a7010162
snowykte0426 <snowykte0426@naver.com>
2024-12-26 17:44:57 +0900

    refactor: 최대값,최저값 적용
    학번이 1101~3418의 범위 안에서 입력될 수 있도록 어노테이션을 적용하였습니다
    
    Related to: #1, #15

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/dto/request/CheckInStatusSwitchRequest.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/dto/request/CheckInStatusSwitchRequest.kt
@@ -1,7 +1,12 @@
 package rainbowfriends.daramserverv2.domain.checkin.dto.request
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 
 class CheckInStatusSwitchRequest {
-    @field:NotNull val studentId: Short? = null
+    @field:NotNull
+    @field:Min(1101)
+    @field:Max(3418)
+    val studentId: Short? = null
 }
