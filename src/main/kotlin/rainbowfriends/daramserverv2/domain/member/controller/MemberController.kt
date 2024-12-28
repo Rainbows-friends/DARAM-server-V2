d3413b16a04c3878b72d63fd58e7e852ddea4cb4
KIM TAEEUN <snowykte0426@naver.com>
2024-12-28 22:14:59 +0900

    feat: 호실 변경 엔드포인트 정의
    특정 사용자의 호실을 변경하는 엔드포인트를 정의하였습니다
    
    Resolves: #58

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/member/controller/MemberController.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/member/controller/MemberController.kt
@@ -1,18 +1,19 @@
 package rainbowfriends.daramserverv2.domain.member.controller
 
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import rainbowfriends.daramserverv2.domain.member.service.CurrentMemberInqueryService
-import rainbowfriends.daramserverv2.domain.member.service.MemberInqueryService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import rainbowfriends.daramserverv2.domain.member.dto.request.PatchRoomRequest
+import rainbowfriends.daramserverv2.domain.member.service.CurrentMemberInquiryService
+import rainbowfriends.daramserverv2.domain.member.service.MemberInquiryService
+import rainbowfriends.daramserverv2.domain.member.service.PatchRoomService
 
 @RestController
 @RequestMapping("/member")
 class MemberController(
-    private val allMemberInqueryService: MemberInqueryService,
-    private val currentMemberInqueryService: CurrentMemberInqueryService
+    private val allMemberInquiryService: MemberInquiryService,
+    private val currentMemberInquiryService: CurrentMemberInquiryService,
+    private val patchRoomService: PatchRoomService
 ) {
     @GetMapping  // GET /member
     fun getAllMember(
@@ -22,8 +23,14 @@ class MemberController(
         @RequestParam(required = false) room: Int?,  // ?room={Int}
         @RequestParam(required = false) grade: Int?,  // ?grade={Int}
         @RequestParam(required = false) classNum: Int?  // ?classNum={Int}
-    ) = allMemberInqueryService.getAllMember(id, stay, floor, room, grade, classNum)
+    ) = allMemberInquiryService.getAllMember(id, stay, floor, room, grade, classNum)
 
     @GetMapping("/current")  // GET /member/current
-    fun getCurrentMember(request: HttpServletRequest) = currentMemberInqueryService.getCurrentMember(request)  // HttpServletRequest를 통해 현재 Member 조회
+    fun getCurrentMember(request: HttpServletRequest) = currentMemberInquiryService.getCurrentMember(request)  // HttpServletRequest를 통해 현재 Member 조회
+
+    @PatchMapping("/room")  // PATCH /member/room
+    @ResponseStatus(HttpStatus.NO_CONTENT)  // 204 No Content
+    fun patchRoom(@RequestBody patchRoomRequest: PatchRoomRequest) {  // Request Body로 PatchRoomRequest를 받아서
+        patchRoomService.patchRoom(patchRoomRequest)  // patchRoomService를 통해 Room 정보 수정
+    }
 }
