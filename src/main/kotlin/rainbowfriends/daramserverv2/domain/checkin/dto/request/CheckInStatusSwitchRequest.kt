5e66a726fc538271e8020731b62ec455d5184748
snowykte0426 <snowykte0426@naver.com>
2024-12-30 23:37:34 +0900

    feat: 학번,호실 범호 검증 어노테이션 추가
    더 정밀한 값 검증을 하도록 사용자 지정된 어노테이션을 추가하였습니다
    
    Resolves: #63

--- src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/dto/request/CheckInStatusSwitchRequest.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/domain/checkin/dto/request/CheckInStatusSwitchRequest.kt
@@ -1,12 +1,10 @@
 package rainbowfriends.daramserverv2.domain.checkin.dto.request
 
-import jakarta.validation.constraints.Max
-import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
+import rainbowfriends.daramserverv2.global.annotation.ValidStudentId
 
 class CheckInStatusSwitchRequest {
     @field:NotNull  // null이 아닌지 검사
-    @field:Min(1101)  // 1101 이상
-    @field:Max(3418)  // 3418 이하
+    @field:ValidStudentId  // 학번 유효성 검사
     val studentId: Short? = null  // Short 타입의 studentId
 }
