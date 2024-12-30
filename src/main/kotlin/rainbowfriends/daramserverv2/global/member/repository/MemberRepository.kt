adf2389882957b17654262dee6121709aecfed45
snowykte0426 <snowykte0426@naver.com>
2024-12-30 11:25:38 +0900

    fix: 캐싱 키 중복 문제 수정
    Member 정보를 캐싱할 때 키가 중복되어 인덱싱이 잘 되지 않았던 문제가를 수정하였습니다
    
    Related to: #1, #17

--- src/main/kotlin/rainbowfriends/daramserverv2/global/member/repository/MemberRepository.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/member/repository/MemberRepository.kt
@@ -2,12 +2,14 @@ package rainbowfriends.daramserverv2.global.member.repository
 
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import rainbowfriends.daramserverv2.global.member.entity.Member
 
 @Repository
 interface MemberRepository : JpaRepository<Member, Long?> {
-    @Cacheable(value = ["MemberByStudentId"], key = "#grade + #classNum + #number")  // 학년, 반, 번호로 조회하는 캐시
+    @Cacheable(value = ["MemberByStudentId"], key = "T(String).format('%d%d%02d', #grade, #classNum, #number)")  // 학년, 반, 번호로 조회하는 캐시
+    @Query("SELECT m FROM Member m WHERE m.grade = :grade AND m.classNum = :classNum AND m.number = :number")  // 쿼리문
     fun findByGradeAndClassNumAndNumber(grade: Int, classNum: Int, number: Int): Member?
     @Cacheable(value = ["MemberByEmail"], key = "#email")  // 이메일로 조회하는 캐시
     fun findByEmail(email: String?): Member?
