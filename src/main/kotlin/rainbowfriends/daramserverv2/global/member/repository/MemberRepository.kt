27b033af75d6364615e83ddaf7e346b8913474be
snowykte0426 <snowykte0426@naver.com>
2024-12-30 23:34:49 +0900

    fix: 신규 회원 로그인 시 캐시 불일치 문제를 해결하기 위한 캐시 삭제
    이메일을 키로 가진 캐시 데이터의 사용빈도가 관련하여 발생하는 문제보다 우선순위가 낮게 가진다고 판단하여 해당 캐시를 삭제하는것으로 결정하였고 이에 따라 캐시를 삭제하였습니다
    
    Related to: #27

--- src/main/kotlin/rainbowfriends/daramserverv2/global/member/repository/MemberRepository.kt
+++ src/main/kotlin/rainbowfriends/daramserverv2/global/member/repository/MemberRepository.kt
@@ -11,6 +11,5 @@ interface MemberRepository : JpaRepository<Member, Long?> {
     @Cacheable(value = ["MemberByStudentId"], key = "T(String).format('%d%d%02d', #grade, #classNum, #number)")  // 학년, 반, 번호로 조회하는 캐시
     @Query("SELECT m FROM Member m WHERE m.grade = :grade AND m.classNum = :classNum AND m.number = :number")  // 쿼리문
     fun findByGradeAndClassNumAndNumber(grade: Int, classNum: Int, number: Int): Member?
-    @Cacheable(value = ["MemberByEmail"], key = "#email")  // 이메일로 조회하는 캐시
     fun findByEmail(email: String?): Member?
 }
